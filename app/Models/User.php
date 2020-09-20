bcef3e6bdea67c4bfb48017669b7a027b0b8c1bb
ahmaddynugroho <ahmaddwi700@gmail.com>
2020-09-20 14:31:14 +0700

    feat: add TasksController

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Task;
 class User extends Authenticatable
 {
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
     use HasFactory, Notifiable;
 
     /**
@@ -16,18 +22,14 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * The attributes that should be cast to native types.
